71df3b76a0240597814e4c75500d2485a7e04d48
MaelstromInvariant <gemzaidahmad@gmail.com>
2026-09-20 17:24:34 +0530

    Clean up config.h by removing comments
    Removed commented sections for OLED, RGB LEDs, and Encoder.

--- Firmware/keyboard/config.h
+++ Firmware/keyboard/config.h
@@ -1,9 +1,6 @@
 #pragma once
 
-// =========================
 // OLED
-// =========================
-
 #define I2C_DRIVER I2CD1
 #define I2C1_SDA_PIN GP6
 #define I2C1_SCL_PIN GP7
@@ -13,23 +10,10 @@
 #define OLED_DISPLAY_WIDTH 128
 #define OLED_DISPLAY_HEIGHT 32
 #define OLED_BRIGHTNESS 180
-
-// Update frequently enough for the eye animation
 #define OLED_UPDATE_INTERVAL 50
-
-
-// =========================
 // RGB LEDs
-// =========================
-
 #define WS2812_DI_PIN GP3
 #define RGBLIGHT_LED_COUNT 2
-
 #define RGBLIGHT_LIMIT_VAL 100
-
-
-// =========================
 // Encoder
-// =========================
-
 #define ENCODER_MAP_KEY_DELAY 10
